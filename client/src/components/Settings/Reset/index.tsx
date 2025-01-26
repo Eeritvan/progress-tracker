83788527f887db610df6ff58748266da6ba28fc0
eeritvan <eeritvan@proton.me>
2025-01-26 20:37:22 +0200

    feat: added reset everything button to settings

--- client/src/components/Settings/Reset/index.tsx
+++ client/src/components/Settings/Reset/index.tsx
@@ -1,7 +1,18 @@
+import useCardListSlice from '@/store/cardListStore'
+
 const Reset = () => {
+  const resetCards = useCardListSlice((state) => state.resetCards)
+
+  const handleClick = () => {
+    const answer = confirm('Are you sure?')
+    if (answer) resetCards()
+  }
+
   return (
     <div>
-      gg
+      <button onClick={() => handleClick()}>
+        Reset everything
+      </button>
     </div>
   )
 }
