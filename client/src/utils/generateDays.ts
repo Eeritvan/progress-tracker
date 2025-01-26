25b974799140d131e4417e86ac02ed23f9e8b947
eeritvan <eeritvan@proton.me>
2025-01-26 20:07:59 +0200

    fix: date generator now doesnt generate extra dates on sundays

--- client/src/utils/generateDays.ts
+++ client/src/utils/generateDays.ts
@@ -6,13 +6,13 @@ export const generateDates = () => {
   const emptyDaysAtStart = (
     new Date(startDate.setDate(startDate.getDate() - 181)).getDay() + 6) % 7
 
-  for (let i = 181-(7-emptyDaysAtStart); i >= 0; i--) {
+  for (let i = 181-((7-emptyDaysAtStart) % 7); i >= 0; i--) {
     const date = new Date(today)
     date.setDate(today.getDate() - i)
     dates.push(date.toISOString().split('T')[0])
   }
 
-  for (let i = 1; i <= 7-emptyDaysAtStart; i++) {
+  for (let i = 1; i <= (7-emptyDaysAtStart) % 7; i++) {
     const date = new Date(today)
     date.setDate(today.getDate() + i)
     dates.push(date.toISOString().split('T')[0])
