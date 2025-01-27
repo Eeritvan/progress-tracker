34db76cd22e71f79848bcbeeab0635af47936241
eeritvan <eeritvan@proton.me>
2025-01-27 02:40:21 +0200

    fix: cardinfo displays correctly even if the text is too long

--- client/src/components/CardList/CardEntry/CardInfo/index.tsx
+++ client/src/components/CardList/CardEntry/CardInfo/index.tsx
@@ -33,17 +33,15 @@ const CardInfo = ({ icon, name, desc }: CardInfoProps) => {
   const IconComponent = iconComponents[icon]
 
   return (
-    <div className='grid grid-cols-2 grid-rows-2 gap-1'>
+    <div className='flex gap-x-2 flex-1 min-w-0'>
       <div className='flex justify-center items-center row-span-2 bg-iconbg
-        aspect-square rounded-xl h-14'
+        aspect-square rounded-xl h-14 shrink-0'
       >
         <IconComponent size={24} />
       </div>
-      <>
-        {name}
-      </>
-      <div className='col-start-2'>
-        {desc}
+      <div className='flex flex-col justify-center min-w-0'>
+        <div className='font-medium truncate'> {name} </div>
+        {desc && <div className='truncate'> {desc} </div>}
       </div>
     </div>
   )
