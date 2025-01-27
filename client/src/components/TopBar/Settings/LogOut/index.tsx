5ae5b608583d54e6920d376233efe3e689f8b8e3
eeritvan <eeritvan@proton.me>
2025-01-27 19:29:08 +0200

    feat: added logout functionality

--- client/src/components/TopBar/Settings/LogOut/index.tsx
+++ client/src/components/TopBar/Settings/LogOut/index.tsx
@@ -1,7 +1,20 @@
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import { useQueryClient } from '@tanstack/react-query'
+
 const LogOut = () => {
+  const queryClient = useQueryClient()
+  const { removeItem } = useLocalStorage('user-info')
+
+  const handleLogout = () => {
+    const answer = confirm('are you sure?')
+    if (answer) removeItem()
+    queryClient.invalidateQueries({ queryKey: ['token'] })
+  }
+
   return (
-    <>
-    </>
+    <button onClick={() => handleLogout()}>
+      logout
+    </button>
   )
 }
 
