dba65c9d3a10044c348341e3a1e62080a674b680
eeritvan <eeritvan@proton.me>
2025-02-01 01:20:43 +0200

    test: added more component tests

---
+++ client/src/components/MainView/TopBar/NewCardForm/ColorSelector/ColorSelector.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import ColorSelector from './index'
+import '@testing-library/jest-dom'
+
+describe('<ColorSelector /> component', () => {
+  const mock = vi.fn()
+
+  test('renders all colors', () => {
+    render(<ColorSelector register={mock} />)
+    const colorInputs = screen.getAllByRole('radio')
+    expect(colorInputs).toHaveLength(15)
+  })
+
+  test('handles error state properly', () => {
+    const { rerender } = render(<ColorSelector register={mock} />)
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+
+    rerender(<ColorSelector register={mock} error='test error'/>)
+    expect(screen.queryByText('test error')).toBeInTheDocument()
+  })
+})
---
+++ client/src/components/MainView/TopBar/NewCardForm/IconSelector/IconSelector.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import IconSelector from './index'
+import '@testing-library/jest-dom'
+
+describe('<IconSelector /> component', () => {
+  const mock = vi.fn()
+
+  test('renders all icons', () => {
+    render(<IconSelector register={mock} />)
+    const radioInputs = screen.getAllByRole('radio')
+    expect(radioInputs).toHaveLength(11)
+  })
+
+  test('handles error state properly', () => {
+    const { rerender } = render(<IconSelector register={mock} />)
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+
+    rerender(<IconSelector register={mock} error='test error'/>)
+    expect(screen.queryByText('test error')).toBeInTheDocument()
+  })
+
+  test('has correct styling', () => {
+    render(<IconSelector register={mock} />)
+    const radioInputs = screen.getAllByRole('radio')
+
+    radioInputs.forEach(radio => {
+      expect(radio).toHaveClass('appearance-none', 'bg-iconbg',
+        'aspect-square', 'rounded-lg')
+    })
+  })
+})
