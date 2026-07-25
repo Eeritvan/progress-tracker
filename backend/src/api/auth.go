ff3963c83c9739ed9104f994b5ace817cfafdb3d
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-07-25 22:25:49 +0300

    chore: random debugging

--- backend/src/api/auth.go
+++ backend/src/api/auth.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/eeritvan/progress-tracker/src/models"
@@ -12,29 +13,42 @@ import (
 
 // (POST /auth/login)
 func (s *Server) Login(c *echo.Context) error {
+	fmt.Println("here0")
 	body := new(models.Login)
 	if err := c.Bind(&body); err != nil {
+		fmt.Println("1", err)
 		return c.JSON(http.StatusBadRequest, nil)
 	}
 
+	fmt.Println("here1")
 	if err := c.Validate(body); err != nil {
+		fmt.Println("2", err)
 		return c.JSON(http.StatusBadRequest, nil)
 	}
+
+	fmt.Println("here2")
 
 	ctx := c.Request().Context()
 	queryRes, _ := s.queries.Login(ctx, body.Name)
 
+	fmt.Println("here3")
 	if err := bcrypt.CompareHashAndPassword([]byte(queryRes.PasswordHash), []byte(body.Password)); err != nil {
+		fmt.Println("3", err)
 		return c.JSON(http.StatusUnauthorized, nil)
 	}
 
+	fmt.Println("here4")
 	jwtToken, err := utils.GenerateJWT(queryRes.ID.String())
 	if err != nil {
+		fmt.Println("4", err)
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
 
+	fmt.Println("here5")
 	jwtCookie := utils.CreateJWTCookie(jwtToken)
 	c.SetCookie(jwtCookie)
+
+	fmt.Println("here6")
 
 	return c.JSON(http.StatusOK, queryRes)
 }
